141c66fb62a1210bbe0e8988305eea15c758f51b
Keckich <nik.malashko@mail.ru>
2020-09-22 05:31:58 +0300

    Home page and categories

---
+++ WebApplication3/CategoryInitial.cs
@@ -0,0 +1,43 @@
+using System.Linq;
+using WebApplication3.Models;
+
+namespace WebApplication3
+{
+    public class CategoryInitial
+    {
+        public static void Initialize(PostContext context)
+        {
+            if (!context.Categories.Any())
+            {
+                context.Categories.AddRange(
+                    new Category
+                    {
+                        Title = "IT",
+                        Description = "Something about technologies, programming etc."
+                    },
+                    new Category
+                    {
+                        Title = "Studying",
+                        Description = "Everything about school, university or other source of knowledge"
+                    },
+                    new Category
+                    {
+                        Title = "Sport and healthy lifestyle",
+                        Description = "Your ways of how to be healthy"
+                    },
+                    new Category
+                    {
+                        Title = "Nature",
+                        Description = "Everything about the world around us"
+                    },
+                    new Category
+                    {
+                        Title = "Food",
+                        Description = "Your eating preferences"
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
